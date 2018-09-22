1f8f661c84512af3bba7469cdf0db26febfaf1a0
Catherine Bronzo <cbronzo@mac.com>
2018-09-22 21:51:55 +0000

    automatically backed up by learn

--- db/migrate/20180922215128_creat_authors.rb
+++ db/migrate/20180922215128_creat_authors.rb
@@ -1,4 +1,10 @@
-class CreatAuthors < ActiveRecord::Migration
+class CreateAuthors < ActiveRecord::Migration
   def change
+    create_table :authors do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
   end
 end
